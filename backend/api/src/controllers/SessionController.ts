dcba1320e3afa4f05f033227c14fac4df66aeded
vynnydev <vynnyprudencio@gmail.com>
2020-09-23 02:59:45 -0300

    changes in project to typescript

---
+++ backend/api/src/controllers/SessionController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import HidraService from '../services/hidra'
+
+interface IShowBody {
+  email: string;
+  password: String;
+}
+
+class SessionController {
+
+  async store(req: Request, res: Response) {
+    const { email, password } = req.body as IShowBody
+
+    const response = await HidraService.loginUser({ 
+      user: { email, password }
+    })
+
+    return res.json(response)
+    
+  }
+}
+
+export default new SessionController()
---
+++ backend/api/src/routes/index.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express'
+
+import UserController from '../controllers/UserController';
+import PurchaseController from '../controllers/PurchaseController';
+import SessionController from '../controllers/SessionController';
+
+import authMiddleware from '../middleware/auth';
+
+const router = Router()
+
+router.get('/users/:id', UserController.show)
+router.post('/users', UserController.store)
+
+router.post('/sessions', SessionController.store)
+
+router.use(authMiddleware)
+
+router.get('/purchases', PurchaseController.index)
+router.get('/purchases/:id', PurchaseController.show)
+router.post('/purchases', PurchaseController.store)
+
+export default router
